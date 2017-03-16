988d44c9a5ef3ca3494fe2ae59dd8b9d87a5ffdb
eileen_chau <eileen_chau@student.wayland.k12.ma.us>
2017-03-16 15:57:14 -0400

    Missle Constructor

---
+++ ChauHsu/src/Main.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+
+/**
+ * Created by eileen_chau on 3/16/17.
+ */
+public class Main extends JPanel{
+    public static final int FRAMEWIDTH = 1000, FRAMEHEIGHT = 600;
+
+    public Main() {
+        setSize(FRAMEWIDTH, FRAMEHEIGHT);
+        Missle m = new Missle(50, 50, 90);
+    }
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Dodge!");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 22 due to title bar.
+
+        Main panel = new Main();
+        panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
+
+        panel.setFocusable(true);
+        panel.grabFocus();
+
+        window.add(panel);
+        window.setVisible(true);
+        window.setResizable(false);
+    }
+}
