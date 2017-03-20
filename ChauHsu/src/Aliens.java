ac942d678e04651c0cda6f233e08bfa18119a809
natalie_hsu <natalie_hsu@student.wayland.k12.ma.us>
2017-03-20 10:19:36 -0400

    added Aliens and Spaceship classes

---
+++ ChauHsu/src/Aliens.java
@@ -0,0 +1,8 @@
+/**
+ * Created by natalie_hsu on 3/20/17.
+ */
+public class Aliens extends Sprite {
+    public Aliens(int x, int y, int dir){
+        super(x, y, dir);
+    }
+}
---
+++ ChauHsu/src/Spaceship.java
@@ -0,0 +1,8 @@
+/**
+ * Created by natalie_hsu on 3/20/17.
+ */
+public class Spaceship extends Sprite {
+    public Spaceship(int x, int y, int dir){
+        super(x, y, dir);
+    }
+}
