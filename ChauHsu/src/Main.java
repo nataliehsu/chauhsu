0c7441e19bc72f61d2a45f39a79b0156ff83dd74
eileen_chau <eileen_chau@student.wayland.k12.ma.us>
2017-03-20 10:31:23 -0400

    main stuff

--- ChauHsu/src/Main.java
+++ ChauHsu/src/Main.java
@@ -1,4 +1,9 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  * Created by eileen_chau on 3/16/17.
@@ -7,11 +12,62 @@ public class Main extends JPanel{
 
     public static final int FRAMEWIDTH = 1000, FRAMEHEIGHT = 600;
     private World theWorld;
+    private Timer timer;
+    private boolean[] keys;
 
     public Main() {
         setSize(FRAMEWIDTH, FRAMEHEIGHT);
         theWorld = new World(FRAMEWIDTH, FRAMEHEIGHT);
         Missle m = new Missle(50, 50, 90, theWorld);
+
+        timer = new Timer(40, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                //move the frog
+                if(keys[KeyEvent.VK_W]){
+                    keys[KeyEvent.VK_W] = false; //probably.
+                }
+                if(keys[KeyEvent.VK_A]){
+                    keys[KeyEvent.VK_A] = false; //probably.
+                }
+                if(keys[KeyEvent.VK_D]){
+                    keys[KeyEvent.VK_D] = false; //probably.
+                }
+                if(keys[KeyEvent.VK_S]){
+                    keys[KeyEvent.VK_S] = false;
+                }
+                if(keys[KeyEvent.VK_R]){
+                }
+
+                repaint();
+            }
+        });
+        timer.start();
+
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                keys[keyEvent.getKeyCode()] = true;
+            }
+
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {
+                keys[keyEvent.getKeyCode()] = false;
+            }
+        });
+
+    }
+
+    //Our paint method.
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+
     }
 
     public static void main(String[] args) {
