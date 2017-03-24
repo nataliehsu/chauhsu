73e3c3439bb15343af0ae69258fbefe68ff7aaf6
natalie_hsu <natalie_hsu@student.wayland.k12.ma.us>
2017-03-24 09:41:29 -0400

    edited Missle Class to work and added lives and kills. Still need to remove alien and spaceShip once missle hits.

--- ChauHsu/src/Main.java
+++ ChauHsu/src/Main.java
@@ -72,6 +72,7 @@ public class Main extends JPanel{
             public void keyTyped(KeyEvent keyEvent) {
                 int code = keyEvent.getKeyChar();
                 if(code == 'r'){
+                    System.out.println(" ");
                 }
             }
 
