32edb525cfcc7308d7e77d8fd81cd75ef4e3106f
natalie_hsu <natalie_hsu@student.wayland.k12.ma.us>
2017-04-03 08:14:52 -0400

    added Med

---
+++ ChauHsu/src/Med.java
@@ -0,0 +1,9 @@
+/**
+ * Created by natalie_hsu on 4/3/17.
+ */
+public class Med extends Sprite{
+    public Med(int x, int y, int dir){
+        super(x, y, dir);
+        setPic("med.png", NORTH);
+    }
+}
